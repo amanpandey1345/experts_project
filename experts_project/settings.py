e12ac4285dfa395191274c644625c8612ef18f18
amanpandey1345 <70140546+amanpandey1345@users.noreply.github.com>
2023-06-06 18:30:56 +0530

    asas

--- experts_project/settings.py
+++ experts_project/settings.py
@@ -118,10 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'experts_app/static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'experts_app/static'),
-]
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "experts_app/static")
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "experts_app/static"),]
 # Default primary key field type 
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
